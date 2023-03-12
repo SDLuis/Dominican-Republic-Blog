af59b0c333557bb8d22925f8558b5415eab2b21b
Luis <Luistejeda0011@gmail.com>
2023-03-12 14:58:21 -0300

    Navbar added and Card component removed

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home () {
+	return (
+		<div className='flex justify-center'>
+			<p>Home</p>
+		</div>
+	)
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar () {
+	return (
+		<div className='w-full px-80 py-8 flex justify-around'>
+			<a href='/paradise' className='text-lg font-medium'>Paradise</a>
+			<a href='/' className='font-semibold text-2xl mt-2'>Dominican Republic</a>
+			<a href='/history' className='text-lg font-medium'>History</a>
+		</div>
+	)
+}
