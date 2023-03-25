89306517cac83e5c112ba039e5d8f0997ef91021
Luis <Luistejeda0011@gmail.com>
2023-03-25 18:47:59 -0300

    Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 export default function Navbar () {
 	return (
-		<div className='w-full px-80 py-8 flex justify-around'>
-			<a href='/paradise' className='text-lg font-medium'>Paradise</a>
-			<a href='/' className='font-semibold text-2xl mt-2'>Dominican Republic</a>
-			<a href='/history' className='text-lg font-medium'>History</a>
+		<div className='w-full px-80 py-20 flex justify-around'>
+			<a href='/paradise' className='text-2xl font-medium'>Paradise</a>
+			<a href='/' className='font-semibold text-4xl mt-2'>Dominican Republic</a>
+			<a href='/history' className='text-2xl font-medium'>History</a>
 		</div>
 	)
 }
